f32e53ee1b4af090a557c3f9d5c887cc7268cd55
Jansey Scofield <163556555+JanseyScofield@users.noreply.github.com>
2026-01-28 08:29:34 -0300

    Delete app/services/paddle_engines directory

--- app/services/paddle_engines/paddle_engine.py
+++
@@ -1,9 +0,0 @@
-# Classe base das engines de paddle, pois, caso a gente queira trocar futuramente, basta ela implementar 
-# dessa classe que o código do main funcionará igualmente
-
-from abc import ABC, abstractmethod
-
-class PaddleEngine(ABC):
-    @abstractmethod
-    def process(self, data):
-        pass

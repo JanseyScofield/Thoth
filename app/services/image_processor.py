c5d3324197c62e2fc68a998490fa25371b0e0da9
Jansey Scofield <163556555+JanseyScofield@users.noreply.github.com>
2026-01-28 10:46:24 -0300

    Delete app/services/image_processor.py

--- app/services/image_processor.py
+++
@@ -1,8 +0,0 @@
-# Classe que vai receber a imagem e fazer todo o pré-tratamento para passar ao paddle.
-
-class ImageProcessor:
-    def __init__(self):
-        pass
-
-    def process_image(self):
-        pass

1183045424b875e7ce34e30a1f3fd7e1a05b8816
Jansey Scofield <163556555+JanseyScofield@users.noreply.github.com>
2026-01-28 13:52:09 -0300

    Add logic to read qrcode

--- app/services/receipt_service.py
+++ app/services/receipt_service.py
@@ -1,13 +1,30 @@
-# Classe que orquestra a chamada dos serviços, um facade que recebe um OCR no construtor.
-# Ele tem o método que vai ser chamado na controller, vai chamar os serviços e retornar o resultado.
-
 from app.services.ocr_engines.ocr_engine import OcrEngine
 from app.services.processors.processor import Processor
+import cv2
 
 class ReceiptService:
     def __init__(self, ocr_engine : OcrEngine, processor : Processor):
         self.__ocr_engine = ocr_engine
         self.__processor = processor
+        self.__qrcode_detector = cv2.QRCodeDetector()
 
-    def process_cupon(self, cupon : bytes):
-        return self.__processor.process_to_ocr(cupon)
+    def process_cupon(self, cupon : bytes) -> dict:
+        img_processed_qrcode = self.__processor.process_to_qrcode(cupon)
+        qrcode_data = self.__get_by_qrcode(img_processed_qrcode)
+        if qrcode_data:
+            return {"data": qrcode_data, "message": "QR code detected successfully"}
+        
+        #Ainda é necessário a lógica do ocr e extração
+        img_processed_ocr = self.__get_by_ocr(cupon)
+        ocr_data = self.__get_by_ocr(cupon)
+        ## Após isso ele extrai os dados do ocr
+        if ocr_data:
+            return {"data": ocr_data, "message": "Ocr engine processed successfully"}
+        return {"data": None, "message": "Ocr engine not processed"}
+
+    def __get_by_qrcode(self, img_processed_qrcode : bytes):
+        qrcode_data, points, _ = self.__qrcode_detector.detectAndDecode(img_processed_qrcode)
+        return qrcode_data
+
+    def __get_by_ocr(self, image : bytes):
+        return []

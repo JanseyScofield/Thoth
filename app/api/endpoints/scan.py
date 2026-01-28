c597d98592fac60feff4bdcdaad321682d7860e4
Jansey Scofield <163556555+JanseyScofield@users.noreply.github.com>
2026-01-28 10:57:22 -0300

    Add dependencies in api

--- app/api/endpoints/scan.py
+++ app/api/endpoints/scan.py
@@ -1,18 +1,23 @@
 #Classe que contém os endpoints do scan
 
 from fastapi import APIRouter, File, UploadFile
+from app.services.receipt_service import ReceiptService
+from app.services.ocr_engines.paddle_engine import PaddleEngine
+from app.services.processors.image_processor import ImageProcessor
 
 router = APIRouter()
+ocr_engine = PaddleEngine()
+processor = ImageProcessor()
+receipt_service  = ReceiptService(ocr_engine=ocr_engine, processor=processor)
 
 @router.post("/scan")
 async def scan(file: UploadFile = File(...)):
     try:       
        content = await file.read()
-       
+       cupon_response = receipt_service.process_cupon(content)
        return {
-           "filename": file.filename,
-           "content_type": file.content_type,
-           "tamanho_bytes": len(content)
-        }
+        "length_before_pre-processing": len(content),
+        "length_after_pre-processing": len(cupon_response)
+       }
     except Exception as e:
         return {"error": str(e)}
